cc430613e891fe7003acbd39bf5be0715158b932
ArkBash <j-bash@protonmail.com>
2020-09-16 19:14:37 +0200

    Send boost the seconde lap during the longest segment.

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ thrust = 100
 boost_used = False
 checkpoints = []
 record_lap = True
+longer_segment_calculated = False
+index_to_boost_to = None
 
 # game loop
 while True:
@@ -17,11 +19,34 @@ while True:
     # next_checkpoint_angle: angle between your pod orientation and the direction of the next checkpoint
     x, y, next_checkpoint_x, next_checkpoint_y, next_checkpoint_dist, next_checkpoint_angle = [int(i) for i in input().split()]
     opponent_x, opponent_y = [int(i) for i in input().split()]
+
+    if not record_lap and not boost_used and not longer_segment_calculated:
+        longest_distance = None
+        for idx, checkpoint in enumerate(checkpoints):
+            x = 0
+            y = 0
+            if idx < (len(checkpoints) - 1):
+                x = math.fabs(checkpoint["x"] - checkpoints[idx + 1]["x"])
+                y = math.fabs(checkpoint["y"] - checkpoints[idx + 1]["y"])
+            else:
+                x = math.fabs(checkpoint["x"] - checkpoints[0]["x"])
+                y = math.fabs(checkpoint["y"] - checkpoints[0]["y"])
+            dist = math.sqrt(x**2 + y**2)
+            if not longest_distance or dist > longest_distance:
+                longest_distance = dist
+                if idx < (len(checkpoints) - 1):
+                    index_to_boost_to = idx + 1
+                else:
+                    index_to_boost_to = 0
+        longer_segment_calculated = True
+
     if record_lap:
         if len(checkpoints) == 0:
             checkpoints.append({"x": next_checkpoint_x, "y": next_checkpoint_y})
-        elif checkpoints[len(checkpoints) - 1]["x"] != next_checkpoint_x or checkpoints[len(checkpoints) - 1]["y"] != next_checkpoint_y:
-            if next_checkpoint_x == checkpoints[0]["x"] and next_checkpoint_y == checkpoints[0]["y"]:
+        elif checkpoints[len(checkpoints) - 1]["x"] != next_checkpoint_x or \
+                checkpoints[len(checkpoints) - 1]["y"] != next_checkpoint_y:
+            if next_checkpoint_x == checkpoints[0]["x"] \
+                    and next_checkpoint_y == checkpoints[0]["y"]:
                 record_lap = False
             else:
                 checkpoints.append({"x": next_checkpoint_x, "y": next_checkpoint_y})
@@ -32,11 +57,16 @@ while True:
     elif next_checkpoint_angle > 30 or next_checkpoint_angle < -30:
         thrust = int(100 - ((math.fabs(next_checkpoint_angle) - 30) * 100 // 60))
     else:
-        if next_checkpoint_dist > 5000 and not boost_used and next_checkpoint_angle < 10 and next_checkpoint_angle > -10:
-            thrust = "BOOST"
-            boost_used = True
-        else:
-            thrust = 100
+        thrust = 100
+
+    if not record_lap and \
+            not boost_used and \
+            math.fabs(next_checkpoint_angle) < 10 and \
+            next_checkpoint_x == checkpoints[index_to_boost_to]["x"] and \
+            next_checkpoint_y == checkpoints[index_to_boost_to]["y"] and \
+            next_checkpoint_dist > 5000:
+        thrust = "BOOST"
+        boost_used = True
 
     print("record_lap: " + str(record_lap), file=sys.stderr, flush=True)
     for checkpoint in checkpoints:
